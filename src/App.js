a182769a96e8dff86c3e68b81eb04933bae73ce0
sravaniNarra <sravanin@worthitcs.com>
2024-03-15 17:58:03 +0530

    latest changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{color:"red"}}>Hello Bmc</h1>
+      <Homepage />
+
+    </div>
+  );
+}
+
+export default App;
